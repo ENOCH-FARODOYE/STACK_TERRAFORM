1f922ede295ede27103b0c23343d2e2d74c9bfc6
Enoch Farodoye <enochfarodoye@gmail.com>
2025-11-20 13:08:37 -0500

    launch template with WordPress bootstrap script (#11)

--- BLOG/data.tf
+++ BLOG/data.tf
@@ -1,9 +1,25 @@
 # ========================================
-# Data Sources - Pull from Management Account SSM
+# Data Sources
 # ========================================
 
-# Get database password from existing SSM parameter
+# Get database password from Management Account SSM
 data "aws_ssm_parameter" "db_password" {
   provider = aws.management
   name     = "/enoch-blog/db-password"
+}
+
+# Get latest Amazon Linux 2023 AMI
+data "aws_ami" "amazon_linux_2023" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 }
