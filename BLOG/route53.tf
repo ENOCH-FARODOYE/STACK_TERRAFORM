c643427f690158086f677a1ee361290915ff0a01
Enoch Farodoye <enochfarodoye@gmail.com>
2025-11-20 12:03:15 -0500

    Route53_DNS_Configuration for enoch-stack.com (#9)

---
+++ BLOG/route53.tf
@@ -0,0 +1,23 @@
+# ========================================
+# Get Hosted Zone (if zone ID not provided)
+# ========================================
+data "aws_route53_zone" "main" {
+  count        = var.hosted_zone_id == "" ? 1 : 0
+  name         = var.domain_name
+  private_zone = false
+}
+
+# ========================================
+# Route53 A Record (Alias to ALB)
+# ========================================
+resource "aws_route53_record" "main" {
+  zone_id = var.hosted_zone_id != "" ? var.hosted_zone_id : data.aws_route53_zone.main[0].zone_id
+  name    = var.domain_name
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.main.dns_name
+    zone_id                = aws_lb.main.zone_id
+    evaluate_target_health = true
+  }
+}
