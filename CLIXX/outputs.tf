d559a830dc8b7708ae0bed07f17177df1223d857
Enoch Farodoye <enochfarodoye@gmail.com>
2025-11-22 15:51:30 -0500

    Added ALB config and updated outputs (#19)

--- CLIXX/outputs.tf
+++ CLIXX/outputs.tf
@@ -100,3 +100,33 @@ output "rds_address" {
   description = "RDS Address (without port)"
   value       = aws_db_instance.main.address
 }
+# ALB Outputs
+output "alb_id" {
+  description = "ALB ID"
+  value       = aws_lb.main.id
+}
+
+output "alb_arn" {
+  description = "ALB ARN"
+  value       = aws_lb.main.arn
+}
+
+output "alb_dns_name" {
+  description = "ALB DNS Name"
+  value       = aws_lb.main.dns_name
+}
+
+output "alb_zone_id" {
+  description = "ALB Hosted Zone ID"
+  value       = aws_lb.main.zone_id
+}
+
+output "target_group_arn" {
+  description = "Target Group ARN"
+  value       = aws_lb_target_group.main.arn
+}
+
+output "target_group_name" {
+  description = "Target Group Name"
+  value       = aws_lb_target_group.main.name
+}
