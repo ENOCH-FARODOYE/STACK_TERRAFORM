1ad16ddd567722f9622aa00ab37e9062a6724ef6
ENOCH-FARODOYE <enochfarodoye@gmail.com>
2025-11-22 13:51:20 -0500

    clixx_elastic_file_system

---
+++ CLIXX/efs.tf
@@ -0,0 +1,37 @@
+# ========================================
+# EFS File System
+# ========================================
+
+resource "aws_efs_file_system" "main" {
+  creation_token = "${var.project_name}-efs"
+  encrypted      = true
+
+  performance_mode = "generalPurpose"
+  throughput_mode  = "bursting"
+
+  lifecycle_policy {
+    transition_to_ia = "AFTER_30_DAYS"
+  }
+
+  tags = {
+    Name = "${var.project_name}-efs"
+  }
+}
+
+# ========================================
+# EFS Mount Targets
+# ========================================
+
+# Mount Target in Subnet 1
+resource "aws_efs_mount_target" "subnet_1" {
+  file_system_id  = aws_efs_file_system.main.id
+  subnet_id       = aws_subnet.subnet_1.id
+  security_groups = [aws_security_group.efs.id]
+}
+
+# Mount Target in Subnet 2
+resource "aws_efs_mount_target" "subnet_2" {
+  file_system_id  = aws_efs_file_system.main.id
+  subnet_id       = aws_subnet.subnet_2.id
+  security_groups = [aws_security_group.efs.id]
+}
