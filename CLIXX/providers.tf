952b9d364e09fb40db258aa956b6c703d91c420c
ENOCH-FARODOYE <enochfarodoye@gmail.com>
2025-12-09 18:37:53 -0500

    Comment out AWS profile for Jenkins - use environment variables

--- CLIXX/providers.tf
+++ CLIXX/providers.tf
@@ -1,12 +1,10 @@
 ##############################################################################
 # AWS Providers - Management and Dev Accounts
 ##############################################################################
-
 # Management Account Provider
 provider "aws" {
   region  = var.environment_config.aws_region
-  profile = var.aws_profile
-
+  # profile = var.aws_profile  # Commented out for Jenkins - uses environment variables
   default_tags {
     tags = merge(
       var.common_tags,
@@ -16,18 +14,15 @@ provider "aws" {
     )
   }
 }
-
 # Dev Account Provider (assumes role)
 provider "aws" {
   alias   = "dev"
   region  = var.environment_config.aws_region
-  profile = var.aws_profile
-
+  # profile = var.aws_profile  # Commented out for Jenkins - uses environment variables
   assume_role {
     role_arn     = "arn:aws:iam::${var.environment_config.dev_account_id}:role/${var.environment_config.engineering_role_name}"
     session_name = "terraform-${var.environment_config.project_name}-${var.environment}"
   }
-
   default_tags {
     tags = merge(
       var.common_tags,
