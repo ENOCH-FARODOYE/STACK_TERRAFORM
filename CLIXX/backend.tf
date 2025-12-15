a7f4ed84afd5f013385d8b48ce574563c7eb080a
ENOCH-FARODOYE <enochfarodoye@gmail.com>
2025-12-15 16:29:59 -0500

    Revert backend - DynamoDB now in Automation account

--- CLIXX/backend.tf
+++ CLIXX/backend.tf
@@ -5,8 +5,5 @@ terraform {
     region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "clixx-terraform-locks"
-    
-    # Assume Management account role to access S3 and DynamoDB
-    role_arn       = "arn:aws:iam::978820380225:role/ClixxSSMParameterAccessRole"
   }
 }
