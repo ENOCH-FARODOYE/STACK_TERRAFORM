ff242961b573bd52e3b3dce13995d71550f1a4f8
ENOCH-FARODOYE <enochfarodoye@gmail.com>
2025-12-15 16:25:26 -0500

    Add role assumption to backend for cross-account access

--- CLIXX/backend.tf
+++ CLIXX/backend.tf
@@ -5,5 +5,8 @@ terraform {
     region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "clixx-terraform-locks"
+    
+    # Assume Management account role to access S3 and DynamoDB
+    role_arn       = "arn:aws:iam::978820380225:role/ClixxSSMParameterAccessRole"
   }
 }
