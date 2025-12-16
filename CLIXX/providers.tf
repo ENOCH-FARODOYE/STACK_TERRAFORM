9eb2739853790d0cfd0bf85c68c4e01498ed084d
ENOCH-FARODOYE <enochfarodoye@gmail.com>
2025-12-16 11:19:37 -0500

    Update providers - default points to Management for Route53/SSM

--- CLIXX/providers.tf
+++ CLIXX/providers.tf
@@ -1,10 +1,16 @@
 ##############################################################################
 # AWS Providers - Management and Dev Accounts
 ##############################################################################
-# Management Account Provider
+
+# Management Account Provider (for Route53, SSM)
 provider "aws" {
-  region  = var.environment_config.aws_region
-  # profile = var.aws_profile 
+  region = var.environment_config.aws_region
+  
+  assume_role {
+    role_arn     = "arn:aws:iam::978820380225:role/TerraformManagementAccess"
+    session_name = "terraform-clixx"
+  }
+  
   default_tags {
     tags = merge(
       var.common_tags,
@@ -14,15 +20,17 @@ provider "aws" {
     )
   }
 }
-# Dev Account Provider (assumes role)
+
+# Dev Account Provider (assumes Engineer role)
 provider "aws" {
-  alias   = "dev"
-  region  = var.environment_config.aws_region
-  # profile = var.aws_profile  # Commented out for Jenkins - uses environment variables
+  alias  = "dev"
+  region = var.environment_config.aws_region
+  
   assume_role {
     role_arn     = "arn:aws:iam::${var.environment_config.dev_account_id}:role/${var.environment_config.engineering_role_name}"
     session_name = "terraform-${var.environment_config.project_name}-${var.environment}"
   }
+  
   default_tags {
     tags = merge(
       var.common_tags,
